4d2f14e922fb5b2e09a63eb4d97fc947b2d561c4
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-06-11 01:25:06 +0900

    Modify argment message

--- server/public/ts/greet.ts
+++ server/public/ts/greet.ts
@@ -2,7 +2,6 @@ import init, { greet } from '../pkg/wasm_deno.js';
 async function run() {
   const file = await fetch('../public/pkg/wasm_deno_bg.wasm'); 
   await init(file);
-  greet('hello');
+  greet('nabezokodaikon');
 }
 run();
-
